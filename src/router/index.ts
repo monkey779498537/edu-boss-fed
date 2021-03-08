66893c979a20089b00c8425caa97d486c8c2bd41
monkey779498537 <65905546+monkey779498537@users.noreply.github.com>
2021-03-08 20:59:19 +0800

    课程管理页面基本样式

--- src/router/index.ts
+++ src/router/index.ts
@@ -72,6 +72,11 @@ const routes: Array<RouteConfig> = [
         name: 'alloc-menu',
         component: () => import(/* webpackChunName： 'alloc-menu' */ '@/views/role/alloc-menu.vue'),
         props: true // 将路由路径参数映射到组件的props中
+      },
+      {
+        path: '/course/created',
+        name: 'course-create',
+        component: () => import(/* webpackChunName: 'course-create' */ '@/views/course/create.vue')
       }
     ]
   },
